e775d96f9b52202bd6c408012382b5856019e72c
alkaz07 <alkaz07@yandex.ru>
2024-04-04 11:30:08 +0300

    задача решена, но в виде 1 огромной громоздкой функции с кучей отладочных выводов

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,69 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //String prg = inputProg();
-        String prg = "LSSSRSRSSSSLSLLLL";
-        runProg1(prg);
+        //String prg = "LSSSRSRSSSSLSLLLL";
+
+        runProg2("LSSSS");
+    }
+
+    public static void runProg2(String prog) {
+        int maxCountS = 10;
+        int[][] matrix = new int[2*maxCountS+1][2*maxCountS+1];
+
+        int directionGrad = 0;
+        int x =maxCountS;
+        int y =maxCountS;
+        int countS=0;
+        boolean flag=false;
+
+        matrix[x][y] = 1;       //оставить след на карте
+        printMatr(matrix);
+        for (char symbol : prog.toCharArray()) {
+            if (symbol == 'R') {//одинарные кавычки задают значение типа char - один символ
+                System.out.println("робот поворачивает направо");
+                directionGrad -=90;
+                if(directionGrad == -90)
+                    directionGrad= 270;
+                System.out.println("направление в градусах = "+directionGrad);
+
+            }
+            if (symbol == 'L') {//одинарные кавычки задают значение типа char - один символ
+                System.out.println("робот поворачивает налево");
+                directionGrad +=90;
+                if(directionGrad == 360)
+                    directionGrad= 0;
+                System.out.println("направление в градусах = "+directionGrad);
+            }
+            if (symbol == 'S') {//одинарные кавычки задают значение типа char - один символ
+                System.out.println("робот идет вперед");
+                if(directionGrad==0){
+                    x++;
+                }
+                if(directionGrad==90){
+                    y++;
+                }
+                if(directionGrad==180){
+                    x--;
+                }
+                if(directionGrad==270){
+                    y--;
+                }
+                countS++;
+                matrix[x][y] ++;   //оставить след
+                if(matrix[x][y]>1) {
+                    System.out.println("робот уже был здесь! Пройдено " + countS + " шагов");
+                    flag=true;
+                    break;
+                }
+                System.out.println("x = "+x+" y = "+y);
+            }
+        }
+        printMatr(matrix);
+
+        if(flag)
+            System.out.println("Ответ "+countS);
+        else
+            System.out.println("Ответ -1");
     }
 
     public static void runProg1(String prog) {
@@ -71,5 +132,14 @@ public class Main {
         System.out.println("Ввведите программу робота");
         String s = scanner.next();
         return s;
+    }
+
+    public static void printMatr(int[][] matr){
+        for (int i = 0; i < matr.length; i++) {
+            for (int j = 0; j <  matr[i].length; j++) {
+                System.out.printf("%2d", matr[i][j]);   //форматируем для красивых ровных столбцов
+            }
+            System.out.println();
+        }
     }
 }
