69f0f31fa88a20793d395e2dfda72476a6037903
alkaz07 <alkaz07@yandex.ru>
2024-04-04 12:42:48 +0300

    задача решена в виде класса робот с кучей функций

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,19 @@ public class Main {
         //String prg = inputProg();
         //String prg = "LSSSRSRSSSSLSLLLL";
 
-        runProg2("LSSSS");
+        runProg3("LSSSS");
+        runProg3("SSLSLSLSSRSRS");
+        //runProg3("LSSSSSSSSSSSSSSSSSSSSS");
+        runProg3("LSSSRSSSSRSSSSLSLLLL");
+    }
+
+    public static void runProg3(String prog) {
+        int maxCountS = 10;
+        int[][] matrix = new int[2*maxCountS+1][2*maxCountS+1];
+        Robot k79 = new Robot(maxCountS, maxCountS, 0, matrix);
+        int res = k79.runProgramUntilCross(prog);
+        System.out.println(res);
+        printMatr(matrix);
     }
 
     public static void runProg2(String prog) {
@@ -141,5 +153,79 @@ public class Main {
             }
             System.out.println();
         }
+    }
+}
+
+class Robot{
+    int x,y;
+    int directionGrad;
+    int countS=0;
+
+    int[][] matrix;
+
+    public Robot(int x, int y, int directionGrad, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.directionGrad = directionGrad;
+        this.matrix = matrix;
+    }
+
+    public void turnLeft(){
+        //directionGrad +=90;
+        //if(directionGrad == 360)
+        //    directionGrad= 0;
+        directionGrad = (directionGrad+90) % 360;
+    }
+
+    public void turnRight(){
+        //directionGrad -=90;
+        //if(directionGrad == -90)
+        //    directionGrad= 270;
+        directionGrad = (directionGrad-90 + 360) % 360;
+    }
+
+    public void stepForward(){
+        if(directionGrad==0){
+            x++;
+        }
+        if(directionGrad==90){
+            y++;
+        }
+        if(directionGrad==180){
+            x--;
+        }
+        if(directionGrad==270){
+            y--;
+        }
+        countS++;
+        matrix[x][y] ++;
+    }
+
+    public void doCommand(char symbol){
+        if (symbol == 'R')
+            turnRight();
+        if (symbol == 'L')
+            turnLeft();
+        if (symbol == 'S')
+            stepForward();
+    }
+
+    public boolean checkIfIWasHere()
+    {
+//        if(matrix[x][y]>1)
+//            return true;
+//        else
+//            return false;
+        return (matrix[x][y]>1);
+    }
+
+    public int runProgramUntilCross(String proga)
+    {
+        for (char symbol : proga.toCharArray()) {
+            doCommand(symbol);
+            if(checkIfIWasHere())
+                return this.countS;
+        }
+        return -1;
     }
 }
